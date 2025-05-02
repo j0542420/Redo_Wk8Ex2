5276628fb6f07a3a54839a35924b1eeb48f1a2ca
Jose Villa <j0542420@amarillocollege.com>
2025-05-01 19:38:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace Redo_Wk8Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a book object and serialize it to a json file
+            Book myBook = new Book
+            {
+                // create a book object with title, author, and year
+                Title = "Dragon Ball",
+                Author = "Akira Toriyama",
+                Year = 1984
+            };
+
+            // serialize the book object to a json file
+            string filePath = "book.json";
+            SerializeBookToJson(myBook, filePath);
+
+            // deserialize the json file to a book object
+            Book deserializedBook = DeserializeJsonToBook(filePath);
+            // print the deserialized book object
+            Console.WriteLine("Title: " + deserializedBook.Title);
+            Console.WriteLine("Author: " + deserializedBook.Author);
+            Console.WriteLine("Year: " + deserializedBook.Year);
+        }
+        // craete a class called Book with properties Title, Author, and Year
+        public class Book 
+        {
+            // properties
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public int Year { get; set; }
+        }
+        // creating method to serialize the book object to a json file
+        public static void SerializeBookToJson(Book book, string filePath)
+        {
+            // serialize the book object to a json string
+            string jsonString = JsonSerializer.Serialize(book);
+            // write the json string to a file
+            File.WriteAllText(filePath, jsonString);
+        }
+        // creating method to deserialize the json file to a book object
+        public static Book DeserializeJsonToBook(string filePath)
+        {
+            // read the json string from the file
+            string jsonString = File.ReadAllText(filePath);
+            // deserialize the json string to a book object
+            Book book = JsonSerializer.Deserialize<Book>(jsonString);
+            return book;
+        }
+    }
+}
